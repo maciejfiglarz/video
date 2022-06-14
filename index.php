a3864b6b049563247c424700f7e0aabae289eb2f
Maciej Figlarz <maciejfiglarz333@gmail.com>
2022-06-14 17:47:41 +0200

    updte

--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 
 require 'vendor/autoload.php';
 
+header("Content-Type: application/json");
+header("Acess-Control-Allow-Origin: *");
+header("Acess-Control-Allow-Methods: POST");
+header("Acess-Control-Allow-Headers: Acess-Control-Allow-Headers,Content-Type,Acess-Control-Allow-Methods, Authorization");
+
 $ffmpeg = FFMpeg\FFMpeg::create();
+
+
+var_dump($_POST);
+
+
+echo php_ini_loaded_file(); ?>
+
