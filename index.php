6783c04660744c9237dd7daa2bfc0bec1efe69a4
Maciej Figlarz <maciejfiglarz333@gmail.com>
2022-06-14 18:27:54 +0200

    updte

--- index.php
+++ index.php
@@ -13,5 +13,24 @@ $ffmpeg = FFMpeg\FFMpeg::create();
 var_dump($_POST,$_FILES);
 
 
+if($FILES){
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["video"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+      if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+      } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+      }
+    }
+}
+
+
 echo php_ini_loaded_file(); ?>
 
